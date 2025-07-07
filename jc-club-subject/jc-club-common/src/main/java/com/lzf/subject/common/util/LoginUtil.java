d0399d4a913501ddc6c63f294e80b57a59c15c85
luozhifeng <228539650@qq.com>
2025-07-07 21:19:57 +0800

    添加了sql语句的控制台输出，方便sql语句的排查

---
+++ jc-club-subject/jc-club-common/src/main/java/com/lzf/subject/common/util/LoginUtil.java
@@ -0,0 +1,19 @@
+package com.lzf.subject.common.util;
+
+
+import com.lzf.subject.common.context.LoginContextHolder;
+
+/**
+ * 用户登录util
+ *
+ * @author: ChickenWing
+ * @date: 2023/11/26
+ */
+public class LoginUtil {
+
+    public static String getLoginId() {
+        return LoginContextHolder.getLoginId();
+    }
+
+
+}
---
+++ jc-club-subject/jc-club-infra/src/main/java/com/lzf/subject/infra/config/MybatisConfiguration.java
@@ -0,0 +1,17 @@
+package com.lzf.subject.infra.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//不用的话直接删除这个注解即可
+@Configuration
+public class MybatisConfiguration {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new MybatisPlusAllSqlLog());
+        return mybatisPlusInterceptor;
+    }
+
+}
