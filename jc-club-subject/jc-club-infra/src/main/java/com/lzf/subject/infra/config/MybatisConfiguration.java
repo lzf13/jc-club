227904c6d781fac327c3019374ea3ed47ccfd24b
luozhifeng <228539650@qq.com>
2025-07-09 21:05:28 +0800

    添加了jenkins

--- jc-club-subject/jc-club-infra/src/main/java/com/lzf/subject/infra/config/MybatisConfiguration.java
+++ jc-club-subject/jc-club-infra/src/main/java/com/lzf/subject/infra/config/MybatisConfiguration.java
@@ -3,7 +3,7 @@ package com.lzf.subject.infra.config;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//不用的话直接删除这个注解即可
+//不用的话直接删除这个注解即可，我觉得在application里面的配置更舒服
 @Configuration
 public class MybatisConfiguration {
 
